da96819b65c7d5c5a415a4eb3a2ecd51a7c19503
Paweł Antończyk <46341979+pawel-ant@users.noreply.github.com>
2021-05-03 22:16:52 +0200

    Add navigation component

--- mynotes/src/components/NavPanel.tsx
+++ mynotes/src/components/NavPanel.tsx
@@ -1,52 +1,163 @@
 import React from 'react';
-import { Drawer } from '@material-ui/core';
 import { useHistory } from 'react-router';
-import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import { createStyles, makeStyles, useTheme, Theme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import LibraryBooksRoundedIcon from '@material-ui/icons/LibraryBooksRounded';
+import LibraryMusicRoundedIcon from '@material-ui/icons/LibraryMusicRounded';
+import ImportContactsRoundedIcon from '@material-ui/icons/ImportContactsRounded';
 
-const useStyles = makeStyles({
-   drawer: {
-      width: '100px',
-   },
-   menuIcon: {
-      cursor: 'pointer',
-      display: 'flex',
-   },
-});
+const drawerWidth = 250;
 
-const NavPanel = () => {
-   const history = useHistory();
-   const [isOpen, setIsOpen] = React.useState(false);
+const useStyles = makeStyles((theme: Theme) =>
+   createStyles({
+      appBar: {
+         position: 'relative',
+         zIndex: theme.zIndex.drawer + 1,
+         background:
+            'linear-gradient(90deg,rgba(111, 128, 255, 1) 100%,rgba(145, 179, 250, 0.770483193277311) 63%)',
+         transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+         }),
+      },
+      appBarShift: {
+         marginLeft: drawerWidth,
+         width: `calc(100% - ${drawerWidth}px)`,
+         transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+         }),
+      },
+      menuButton: {
+         marginRight: 36,
+      },
+      hide: {
+         display: 'none',
+      },
+      drawer: {
+         width: drawerWidth,
+         flexShrink: 0,
+         whiteSpace: 'nowrap',
+      },
+      drawerOpen: {
+         width: drawerWidth,
+         transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+         }),
+      },
+      drawerClose: {
+         transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+         }),
+         overflowX: 'hidden',
+         width: theme.spacing(7) + 1,
+         [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(9) + 1,
+         },
+      },
+      toolbar: {
+         display: 'flex',
+         alignItems: 'center',
+         justifyContent: 'flex-end',
+         padding: theme.spacing(0, 1),
+         // necessary for content to be below app bar
+         ...theme.mixins.toolbar,
+      },
+   }),
+);
+
+const NavPanel: React.FC = () => {
    const classes = useStyles();
-   const redirectTo = (path: string, name: string) => (
-      <div
+   const theme = useTheme();
+   const history = useHistory();
+   const [open, setOpen] = React.useState(false);
+
+   const handleDrawerOpen = () => {
+      setOpen(true);
+   };
+
+   const handleDrawerClose = () => {
+      setOpen(false);
+   };
+
+   const redirectTo = (path: string, icon: React.ReactNode, name: string) => (
+      <ListItem
+         button
          onClick={() => {
             history.push(path);
          }}
       >
-         {name}
-      </div>
+         <ListItemIcon>{icon}</ListItemIcon>
+         <ListItemText primary={name} />
+      </ListItem>
    );
 
    return (
       <div>
-         <div className={classes.menuIcon} onClick={() => setIsOpen(!isOpen)}>
-            <MenuIcon />
-         </div>
+         <CssBaseline />
+         <AppBar
+            position="fixed"
+            className={clsx(classes.appBar, {
+               [classes.appBarShift]: open,
+            })}
+         >
+            <Toolbar>
+               <IconButton
+                  color="inherit"
+                  aria-label="open drawer"
+                  onClick={handleDrawerOpen}
+                  edge="start"
+                  className={clsx(classes.menuButton, {
+                     [classes.hide]: open,
+                  })}
+               >
+                  <MenuIcon />
+               </IconButton>
+               <Typography variant="h6" noWrap>
+                  My Note APP
+               </Typography>
+            </Toolbar>
+         </AppBar>
          <Drawer
-            className={classes.drawer}
-            open={isOpen}
-            onClose={() => {
-               setIsOpen(false);
+            variant="permanent"
+            className={clsx(classes.drawer, {
+               [classes.drawerOpen]: open,
+               [classes.drawerClose]: !open,
+            })}
+            classes={{
+               paper: clsx({
+                  [classes.drawerOpen]: open,
+                  [classes.drawerClose]: !open,
+               }),
             }}
          >
-            <div>
-               <ul>
-                  <li>{redirectTo('/', 'Notes')}</li>
-                  <li>{redirectTo('/articles', 'Articles')}</li>
-                  <li>{redirectTo('/songs', 'Songs')}</li>
-               </ul>
+            <div className={classes.toolbar}>
+               <IconButton onClick={handleDrawerClose}>
+                  {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+               </IconButton>
             </div>
+            <Divider />
+            <List>
+               <li>{redirectTo('/', <LibraryBooksRoundedIcon />, 'Notes')}</li>
+               <li>{redirectTo('/songs', <LibraryMusicRoundedIcon />, 'Songs')}</li>
+               <li>{redirectTo('/articles', <ImportContactsRoundedIcon />, 'Articles')}</li>
+            </List>
          </Drawer>
       </div>
    );
